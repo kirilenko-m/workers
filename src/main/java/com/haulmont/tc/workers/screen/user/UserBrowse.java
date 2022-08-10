0db2fe8872a7472f84c05aa6e445120d8def7a2d
Mikhail Kirilenko <kirilenko@haulmont.com>
2022-08-10 12:14:49 +0400

    Проект создан

---
+++ src/main/java/com/haulmont/tc/workers/screen/user/UserBrowse.java
@@ -0,0 +1,15 @@
+package com.haulmont.tc.workers.screen.user;
+
+import com.haulmont.tc.workers.entity.User;
+import io.jmix.ui.navigation.Route;
+import io.jmix.ui.screen.LookupComponent;
+import io.jmix.ui.screen.StandardLookup;
+import io.jmix.ui.screen.UiController;
+import io.jmix.ui.screen.UiDescriptor;
+
+@UiController("User.browse")
+@UiDescriptor("user-browse.xml")
+@LookupComponent("usersTable")
+@Route("users")
+public class UserBrowse extends StandardLookup<User> {
+}
